6082682ce2498f988d1f59a7a5195f1400449042
Helon21 <helonbentes51@hotmail.com>
2024-04-16 16:28:54 -0300

    feat: add orderStatus class

--- src/main/java/com/compassuol/estagio/springcourse/config/TestConfig.java
+++ src/main/java/com/compassuol/estagio/springcourse/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.compassuol.estagio.springcourse.config;
 
 import com.compassuol.estagio.springcourse.entities.Order;
 import com.compassuol.estagio.springcourse.entities.User;
+import com.compassuol.estagio.springcourse.entities.enums.OrderStatus;
 import com.compassuol.estagio.springcourse.repositories.OrderRepository;
 import com.compassuol.estagio.springcourse.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User u2 = new User(null, "Emilly", "emilly@gmail.com", "9999999", "12345");
         User u3 = new User(null, "Helen", "helen@gmail.com", "9999999","12345");
 
-        Order o1 = new Order(null, Instant.parse("2024-04-16T12:08:12Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2024-04-03T18:07:22Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2024-04-05T16:25:42Z"), u3);
+        Order o1 = new Order(null, Instant.parse("2024-04-16T12:08:12Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2024-04-03T18:07:22Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2024-04-05T16:25:42Z"), OrderStatus.WAITING_PAYMENT, u3);
         userRepository.saveAll(Arrays.asList(u1, u2, u3));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
