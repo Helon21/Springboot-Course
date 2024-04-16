d507933978a296ec2d9564e7f730704ea15c0cbb
Helon21 <helonbentes51@hotmail.com>
2024-04-16 10:58:16 -0300

    created order entity

--- src/main/java/com/compassuol/estagio/springcourse/entities/User.java
+++ src/main/java/com/compassuol/estagio/springcourse/entities/User.java
@@ -3,10 +3,12 @@ package com.compassuol.estagio.springcourse.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Table(name="tb_user")
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     @Id
@@ -16,6 +18,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
 
@@ -67,6 +72,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
