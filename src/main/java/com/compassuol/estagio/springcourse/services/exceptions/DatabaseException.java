060e0a1513b154db81a497b04a6e0471f5dc5d20
Helon21 <helonbentes51@hotmail.com>
2024-04-19 10:47:26 -0300

    created personalized exceptions of delete user

---
+++ src/main/java/com/compassuol/estagio/springcourse/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.compassuol.estagio.springcourse.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+
+}
