6ac78ba1fa94ea5f07b31973b6eb0a5b71fb37f2
Helon21 <helonbentes51@hotmail.com>
2024-04-19 10:53:27 -0300

    created personalized exception to update user

--- src/main/java/com/compassuol/estagio/springcourse/services/UserService.java
+++ src/main/java/com/compassuol/estagio/springcourse/services/UserService.java
@@ -4,6 +4,7 @@ import com.compassuol.estagio.springcourse.entities.User;
 import com.compassuol.estagio.springcourse.repositories.UserRepository;
 import com.compassuol.estagio.springcourse.services.exceptions.DatabaseException;
 import com.compassuol.estagio.springcourse.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
